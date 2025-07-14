52417ca20e14cb0bcd786bc920ffe1442d40cdda
Rahul Bhargava <rahulbot@gmail.com>
2025-07-14 11:56:30 -0400

    fix(gmmp): fixed for second run with correct project

--- scripts/gmmp.py
+++ scripts/gmmp.py
@@ -1,5 +1,6 @@
 import datetime as dt
 
-ALLOWED_PROJECT_IDS = [128, 116, 223]
+# ALLOWED_PROJECT_IDS = [128, 116, 223]
+ALLOWED_PROJECT_IDS = [250]
 START_DATE = dt.date(2025, 5, 6)
 END_DATE = dt.date(2025, 5, 7)
